77a0981684353e367bf6d7cbf88cbbdb18679341
NicolaasDieBaas <nicki@westerncapelabs.com>
2016-06-29 09:21:09 +0200

    add src/ app files

---
+++ src/optout.js
@@ -0,0 +1,28 @@
+go.app = function() {
+    var vumigo = require('vumigo_v02');
+    var App = vumigo.App;
+    var EndState = vumigo.states.EndState;
+
+    var GoNDOH = App.extend(function(self) {
+        App.call(self, 'isl_route');
+        var $ = self.$;
+        var interrupt = true;
+
+        self.init = function() {
+
+        };
+
+        self.add('state_start', function(name) {
+            return new EndState(name, {
+                text: $('Welcome to The Department of Health\'s optout.js'),
+                next: function(choice) {
+                    return 'state_start';
+                }
+            });
+        });
+    });
+
+    return {
+        GoNDOH: GoNDOH
+    };
+}();
