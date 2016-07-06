f3828e810b8abe6e96bdfce9402ab67f9754a69e
NicolaasDieBaas <nicki@westerncapelabs.com>
2016-07-06 15:00:41 +0200

    change package.json

---
+++ go-app-ussd_clinic.js
@@ -0,0 +1,42 @@
+var go = {};
+go;
+
+go.app = function() {
+    var vumigo = require('vumigo_v02');
+    var App = vumigo.App;
+    var EndState = vumigo.states.EndState;
+
+    var GoNDOH = App.extend(function(self) {
+        App.call(self, 'state_start');
+        var $ = self.$;
+
+        self.init = function() {
+
+        };
+
+        self.states.add('state_start', function(name) {
+            return new EndState(name, {
+                text: $('Welcome to The Department of Health\'s clinic.js'),
+                next: function(choice) {
+                    return 'state_start';
+                }
+            });
+        });
+
+    });
+
+    return {
+        GoNDOH: GoNDOH
+    };
+}();
+
+go.init = function() {
+    var vumigo = require('vumigo_v02');
+    var InteractionMachine = vumigo.InteractionMachine;
+    var GoNDOH = go.app.GoNDOH;
+
+
+    return {
+        im: new InteractionMachine(api, new GoNDOH())
+    };
+}();
---
+++ src/ussd_public.js
@@ -0,0 +1,28 @@
+go.app = function() {
+    var vumigo = require('vumigo_v02');
+    var App = vumigo.App;
+    var EndState = vumigo.states.EndState;
+
+    var GoNDOH = App.extend(function(self) {
+        App.call(self, 'state_start');
+        var $ = self.$;
+
+        self.init = function() {
+
+        };
+
+        self.states.add('state_start', function(name) {
+            return new EndState(name, {
+                text: $('Welcome to The Department of Health\'s public.js'),
+                next: function(choice) {
+                    return 'state_start';
+                }
+            });
+        });
+
+    });
+
+    return {
+        GoNDOH: GoNDOH
+    };
+}();
