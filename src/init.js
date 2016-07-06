a7f8187e05cd09647a25872bf9642a7ecbf785e1
NicolaasDieBaas <nicki@westerncapelabs.com>
2016-07-06 13:29:39 +0200

    clear directory structure

--- src/init.js
+++
@@ -1,9 +0,0 @@
-go.init = function() {
-    var vumigo = require('vumigo_v02');
-    var InteractionMachine = vumigo.InteractionMachine;
-    var GoNDOH = go.app.GoNDOH;
-
-    return {
-        im: new InteractionMachine(api, new GoNDOH())
-    };
-}();
