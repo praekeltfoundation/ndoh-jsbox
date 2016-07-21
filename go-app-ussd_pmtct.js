10f8a465ee71367e7ee3bfe7c6f0c4e530f0744e
Mike Jones <mike@westerncapelabs.com>
2016-07-21 09:47:14 +0200

    Add go.init

--- go-app-ussd_pmtct.js
+++ go-app-ussd_pmtct.js
@@ -459,3 +459,14 @@ go.app = function() {
         GoNDOH: GoNDOH
     };
 }();
+
+go.init = function() {
+    var vumigo = require('vumigo_v02');
+    var InteractionMachine = vumigo.InteractionMachine;
+    var GoNDOH = go.app.GoNDOH;
+
+
+    return {
+        im: new InteractionMachine(api, new GoNDOH())
+    };
+}();
