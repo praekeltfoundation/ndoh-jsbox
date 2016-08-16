46da94a314942300998ae34304a51d1d44074b7a
NicolaasDieBaas <nicki@westerncapelabs.com>
2016-08-16 14:57:52 +0200

    create separate service-specific fixture files

---
+++ test/fixtures_stage_based_messaging.js
@@ -0,0 +1,166 @@
+
+module.exports = function() {
+    return [
+
+        // 0:
+        {
+            'request': {
+                'method': 'GET',
+                'params': {
+                    "deprecated": "fixture"
+                },
+                'headers': {
+                    'Authorization': ['Bearer abcde'],
+                    'Content-Type': ['application/json']
+                },
+                'url': 'http://',
+            },
+            'response': {}
+        },
+
+        // 1:
+        {
+            'request': {
+                'method': 'GET',
+                'params': {
+                    "deprecated": "fixture"
+                },
+                'headers': {
+                    'Authorization': ['Bearer abcde'],
+                    'Content-Type': ['application/json']
+                },
+                'url': 'http://',
+            },
+            'response': {}
+        },
+
+        // 2:
+        {
+            'request': {
+                'method': 'GET',
+                'params': {
+                    "deprecated": "fixture"
+                },
+                'headers': {
+                    'Authorization': ['Bearer abcde'],
+                    'Content-Type': ['application/json']
+                },
+                'url': 'http://',
+            },
+            'response': {}
+        },
+
+        // 3:
+        {
+            'request': {
+                'method': 'GET',
+                'params': {
+                    "deprecated": "fixture"
+                },
+                'headers': {
+                    'Authorization': ['Bearer abcde'],
+                    'Content-Type': ['application/json']
+                },
+                'url': 'http://',
+            },
+            'response': {}
+        },
+
+        // 4:
+        {
+            'request': {
+                'method': 'GET',
+                'params': {
+                    "deprecated": "fixture"
+                },
+                'headers': {
+                    'Authorization': ['Bearer abcde'],
+                    'Content-Type': ['application/json']
+                },
+                'url': 'http://',
+            },
+            'response': {}
+        },
+
+        // 5:
+        {
+            'request': {
+                'method': 'GET',
+                'params': {
+                    "deprecated": "fixture"
+                },
+                'headers': {
+                    'Authorization': ['Bearer abcde'],
+                    'Content-Type': ['application/json']
+                },
+                'url': 'http://',
+            },
+            'response': {}
+        },
+
+        // 6:
+        {
+            'request': {
+                'method': 'GET',
+                'params': {
+                    "deprecated": "fixture"
+                },
+                'headers': {
+                    'Authorization': ['Bearer abcde'],
+                    'Content-Type': ['application/json']
+                },
+                'url': 'http://',
+            },
+            'response': {}
+        },
+
+        // 7:
+        {
+            'request': {
+                'method': 'GET',
+                'params': {
+                    "deprecated": "fixture"
+                },
+                'headers': {
+                    'Authorization': ['Bearer abcde'],
+                    'Content-Type': ['application/json']
+                },
+                'url': 'http://',
+            },
+            'response': {}
+        },
+
+        // 8:
+        {
+            'request': {
+                'method': 'GET',
+                'params': {
+                    "deprecated": "fixture"
+                },
+                'headers': {
+                    'Authorization': ['Bearer abcde'],
+                    'Content-Type': ['application/json']
+                },
+                'url': 'http://',
+            },
+            'response': {}
+        },
+
+        // 9:
+        {
+            'request': {
+                'method': 'GET',
+                'params': {
+                    "deprecated": "fixture"
+                },
+                'headers': {
+                    'Authorization': ['Bearer abcde'],
+                    'Content-Type': ['application/json']
+                },
+                'url': 'http://',
+            },
+            'response': {}
+        },
+
+    ];
+};
