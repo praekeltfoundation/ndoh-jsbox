633be1ca0177afb25e9f4bcac733b98bbb43ec4c
NicolaasDieBaas <nicki@westerncapelabs.com>
2016-08-17 16:18:51 +0200

    remove sms_pmtct fixture from fixtures_identity_store.js

--- test/fixtures_identity_store.js
+++ test/fixtures_identity_store.js
@@ -148,26 +148,5 @@ module.exports = function() {
             }
         },
 
-        // 4: optout cb245673-aa41-4302-ac47-00000000001 (sms_pmtct)
-        {
-            "request": {
-                "url": "http://is.localhost:8001/api/v1/optout/",
-                "method": 'POST',
-                "data": {
-                    "optout_type": "stop",
-                    "identity": "cb245673-aa41-4302-ac47-00000000001",
-                    "reason": "unknown",
-                    "address_type": "msisdn",
-                    "address": "+27820000111",
-                    "request_source": "sms_pmtct",
-                    "requestor_source_id": "0170b7bb-978e-4b8a-35d2-662af5b6daee"
-                },
-                "headers": {
-                    "key": "post.optout.27820000111"
-                }
-            },
-            "response": {}
-        },
-
     ];
 };
