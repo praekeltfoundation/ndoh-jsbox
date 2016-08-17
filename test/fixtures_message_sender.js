31cd2b972192d35741f7d5c4c0c654310822905f
NicolaasDieBaas <nicki@westerncapelabs.com>
2016-08-17 10:34:39 +0200

    add fixture placeholder slots for MessageSender service too

--- test/fixtures_message_sender.js
+++ test/fixtures_message_sender.js
@@ -162,5 +162,645 @@ module.exports = function() {
             'response': {}
         },
 
+        // 10:
+        {
+            'request': {
+                'method': 'GET',
+                'params': {
+                    "deprecated": "fixture"
+                },
+                'headers': {
+                    'Authorization': ['Bearer abcde'],
+                    'Content-Type': ['application/json']
+                },
+                'url': 'http://',
+            },
+            'response': {}
+        },
+
+        // 11:
+        {
+            'request': {
+                'method': 'GET',
+                'params': {
+                    "deprecated": "fixture"
+                },
+                'headers': {
+                    'Authorization': ['Bearer abcde'],
+                    'Content-Type': ['application/json']
+                },
+                'url': 'http://',
+            },
+            'response': {}
+        },
+
+        // 12:
+        {
+            'request': {
+                'method': 'GET',
+                'params': {
+                    "deprecated": "fixture"
+                },
+                'headers': {
+                    'Authorization': ['Bearer abcde'],
+                    'Content-Type': ['application/json']
+                },
+                'url': 'http://',
+            },
+            'response': {}
+        },
+
+        // 13:
+        {
+            'request': {
+                'method': 'GET',
+                'params': {
+                    "deprecated": "fixture"
+                },
+                'headers': {
+                    'Authorization': ['Bearer abcde'],
+                    'Content-Type': ['application/json']
+                },
+                'url': 'http://',
+            },
+            'response': {}
+        },
+
+        // 14:
+        {
+            'request': {
+                'method': 'GET',
+                'params': {
+                    "deprecated": "fixture"
+                },
+                'headers': {
+                    'Authorization': ['Bearer abcde'],
+                    'Content-Type': ['application/json']
+                },
+                'url': 'http://',
+            },
+            'response': {}
+        },
+
+        // 15:
+        {
+            'request': {
+                'method': 'GET',
+                'params': {
+                    "deprecated": "fixture"
+                },
+                'headers': {
+                    'Authorization': ['Bearer abcde'],
+                    'Content-Type': ['application/json']
+                },
+                'url': 'http://',
+            },
+            'response': {}
+        },
+
+        // 16:
+        {
+            'request': {
+                'method': 'GET',
+                'params': {
+                    "deprecated": "fixture"
+                },
+                'headers': {
+                    'Authorization': ['Bearer abcde'],
+                    'Content-Type': ['application/json']
+                },
+                'url': 'http://',
+            },
+            'response': {}
+        },
+
+        // 17:
+        {
+            'request': {
+                'method': 'GET',
+                'params': {
+                    "deprecated": "fixture"
+                },
+                'headers': {
+                    'Authorization': ['Bearer abcde'],
+                    'Content-Type': ['application/json']
+                },
+                'url': 'http://',
+            },
+            'response': {}
+        },
+
+        // 18:
+        {
+            'request': {
+                'method': 'GET',
+                'params': {
+                    "deprecated": "fixture"
+                },
+                'headers': {
+                    'Authorization': ['Bearer abcde'],
+                    'Content-Type': ['application/json']
+                },
+                'url': 'http://',
+            },
+            'response': {}
+        },
+
+        // 19:
+        {
+            'request': {
+                'method': 'GET',
+                'params': {
+                    "deprecated": "fixture"
+                },
+                'headers': {
+                    'Authorization': ['Bearer abcde'],
+                    'Content-Type': ['application/json']
+                },
+                'url': 'http://',
+            },
+            'response': {}
+        },
+
+        // 20:
+        {
+            'request': {
+                'method': 'GET',
+                'params': {
+                    "deprecated": "fixture"
+                },
+                'headers': {
+                    'Authorization': ['Bearer abcde'],
+                    'Content-Type': ['application/json']
+                },
+                'url': 'http://',
+            },
+            'response': {}
+        },
+
+        // 21:
+        {
+            'request': {
+                'method': 'GET',
+                'params': {
+                    "deprecated": "fixture"
+                },
+                'headers': {
+                    'Authorization': ['Bearer abcde'],
+                    'Content-Type': ['application/json']
+                },
+                'url': 'http://',
+            },
+            'response': {}
+        },
+
+        // 22:
+        {
+            'request': {
+                'method': 'GET',
+                'params': {
+                    "deprecated": "fixture"
+                },
+                'headers': {
+                    'Authorization': ['Bearer abcde'],
+                    'Content-Type': ['application/json']
+                },
+                'url': 'http://',
+            },
+            'response': {}
+        },
+
+        // 23:
+        {
+            'request': {
+                'method': 'GET',
+                'params': {
+                    "deprecated": "fixture"
+                },
+                'headers': {
+                    'Authorization': ['Bearer abcde'],
+                    'Content-Type': ['application/json']
+                },
+                'url': 'http://',
+            },
+            'response': {}
+        },
+
+        // 24:
+        {
+            'request': {
+                'method': 'GET',
+                'params': {
+                    "deprecated": "fixture"
+                },
+                'headers': {
+                    'Authorization': ['Bearer abcde'],
+                    'Content-Type': ['application/json']
+                },
+                'url': 'http://',
+            },
+            'response': {}
+        },
+
+        // 25:
+        {
+            'request': {
+                'method': 'GET',
+                'params': {
+                    "deprecated": "fixture"
+                },
+                'headers': {
+                    'Authorization': ['Bearer abcde'],
+                    'Content-Type': ['application/json']
+                },
+                'url': 'http://',
+            },
+            'response': {}
+        },
+
+        // 26:
+        {
+            'request': {
+                'method': 'GET',
+                'params': {
+                    "deprecated": "fixture"
+                },
+                'headers': {
+                    'Authorization': ['Bearer abcde'],
+                    'Content-Type': ['application/json']
+                },
+                'url': 'http://',
+            },
+            'response': {}
+        },
+
+        // 27:
+        {
+            'request': {
+                'method': 'GET',
+                'params': {
+                    "deprecated": "fixture"
+                },
+                'headers': {
+                    'Authorization': ['Bearer abcde'],
+                    'Content-Type': ['application/json']
+                },
+                'url': 'http://',
+            },
+            'response': {}
+        },
+
+        // 28:
+        {
+            'request': {
+                'method': 'GET',
+                'params': {
+                    "deprecated": "fixture"
+                },
+                'headers': {
+                    'Authorization': ['Bearer abcde'],
+                    'Content-Type': ['application/json']
+                },
+                'url': 'http://',
+            },
+            'response': {}
+        },
+
+        // 29:
+        {
+            'request': {
+                'method': 'GET',
+                'params': {
+                    "deprecated": "fixture"
+                },
+                'headers': {
+                    'Authorization': ['Bearer abcde'],
+                    'Content-Type': ['application/json']
+                },
+                'url': 'http://',
+            },
+            'response': {}
+        },
+
+        // 30:
+        {
+            'request': {
+                'method': 'GET',
+                'params': {
+                    "deprecated": "fixture"
+                },
+                'headers': {
+                    'Authorization': ['Bearer abcde'],
+                    'Content-Type': ['application/json']
+                },
+                'url': 'http://',
+            },
+            'response': {}
+        },
+
+        // 31:
+        {
+            'request': {
+                'method': 'GET',
+                'params': {
+                    "deprecated": "fixture"
+                },
+                'headers': {
+                    'Authorization': ['Bearer abcde'],
+                    'Content-Type': ['application/json']
+                },
+                'url': 'http://',
+            },
+            'response': {}
+        },
+
+        // 32:
+        {
+            'request': {
+                'method': 'GET',
+                'params': {
+                    "deprecated": "fixture"
+                },
+                'headers': {
+                    'Authorization': ['Bearer abcde'],
+                    'Content-Type': ['application/json']
+                },
+                'url': 'http://',
+            },
+            'response': {}
+        },
+
+        // 33:
+        {
+            'request': {
+                'method': 'GET',
+                'params': {
+                    "deprecated": "fixture"
+                },
+                'headers': {
+                    'Authorization': ['Bearer abcde'],
+                    'Content-Type': ['application/json']
+                },
+                'url': 'http://',
+            },
+            'response': {}
+        },
+
+        // 34:
+        {
+            'request': {
+                'method': 'GET',
+                'params': {
+                    "deprecated": "fixture"
+                },
+                'headers': {
+                    'Authorization': ['Bearer abcde'],
+                    'Content-Type': ['application/json']
+                },
+                'url': 'http://',
+            },
+            'response': {}
+        },
+
+        // 35:
+        {
+            'request': {
+                'method': 'GET',
+                'params': {
+                    "deprecated": "fixture"
+                },
+                'headers': {
+                    'Authorization': ['Bearer abcde'],
+                    'Content-Type': ['application/json']
+                },
+                'url': 'http://',
+            },
+            'response': {}
+        },
+
+        // 36:
+        {
+            'request': {
+                'method': 'GET',
+                'params': {
+                    "deprecated": "fixture"
+                },
+                'headers': {
+                    'Authorization': ['Bearer abcde'],
+                    'Content-Type': ['application/json']
+                },
+                'url': 'http://',
+            },
+            'response': {}
+        },
+
+        // 37:
+        {
+            'request': {
+                'method': 'GET',
+                'params': {
+                    "deprecated": "fixture"
+                },
+                'headers': {
+                    'Authorization': ['Bearer abcde'],
+                    'Content-Type': ['application/json']
+                },
+                'url': 'http://',
+            },
+            'response': {}
+        },
+
+        // 38:
+        {
+            'request': {
+                'method': 'GET',
+                'params': {
+                    "deprecated": "fixture"
+                },
+                'headers': {
+                    'Authorization': ['Bearer abcde'],
+                    'Content-Type': ['application/json']
+                },
+                'url': 'http://',
+            },
+            'response': {}
+        },
+
+        // 39:
+        {
+            'request': {
+                'method': 'GET',
+                'params': {
+                    "deprecated": "fixture"
+                },
+                'headers': {
+                    'Authorization': ['Bearer abcde'],
+                    'Content-Type': ['application/json']
+                },
+                'url': 'http://',
+            },
+            'response': {}
+        },
+
+        // 40:
+        {
+            'request': {
+                'method': 'GET',
+                'params': {
+                    "deprecated": "fixture"
+                },
+                'headers': {
+                    'Authorization': ['Bearer abcde'],
+                    'Content-Type': ['application/json']
+                },
+                'url': 'http://',
+            },
+            'response': {}
+        },
+
+        // 41:
+        {
+            'request': {
+                'method': 'GET',
+                'params': {
+                    "deprecated": "fixture"
+                },
+                'headers': {
+                    'Authorization': ['Bearer abcde'],
+                    'Content-Type': ['application/json']
+                },
+                'url': 'http://',
+            },
+            'response': {}
+        },
+
+        // 42:
+        {
+            'request': {
+                'method': 'GET',
+                'params': {
+                    "deprecated": "fixture"
+                },
+                'headers': {
+                    'Authorization': ['Bearer abcde'],
+                    'Content-Type': ['application/json']
+                },
+                'url': 'http://',
+            },
+            'response': {}
+        },
+
+        // 43:
+        {
+            'request': {
+                'method': 'GET',
+                'params': {
+                    "deprecated": "fixture"
+                },
+                'headers': {
+                    'Authorization': ['Bearer abcde'],
+                    'Content-Type': ['application/json']
+                },
+                'url': 'http://',
+            },
+            'response': {}
+        },
+
+        // 44:
+        {
+            'request': {
+                'method': 'GET',
+                'params': {
+                    "deprecated": "fixture"
+                },
+                'headers': {
+                    'Authorization': ['Bearer abcde'],
+                    'Content-Type': ['application/json']
+                },
+                'url': 'http://',
+            },
+            'response': {}
+        },
+
+        // 45:
+        {
+            'request': {
+                'method': 'GET',
+                'params': {
+                    "deprecated": "fixture"
+                },
+                'headers': {
+                    'Authorization': ['Bearer abcde'],
+                    'Content-Type': ['application/json']
+                },
+                'url': 'http://',
+            },
+            'response': {}
+        },
+
+        // 46:
+        {
+            'request': {
+                'method': 'GET',
+                'params': {
+                    "deprecated": "fixture"
+                },
+                'headers': {
+                    'Authorization': ['Bearer abcde'],
+                    'Content-Type': ['application/json']
+                },
+                'url': 'http://',
+            },
+            'response': {}
+        },
+
+        // 47:
+        {
+            'request': {
+                'method': 'GET',
+                'params': {
+                    "deprecated": "fixture"
+                },
+                'headers': {
+                    'Authorization': ['Bearer abcde'],
+                    'Content-Type': ['application/json']
+                },
+                'url': 'http://',
+            },
+            'response': {}
+        },
+
+        // 48:
+        {
+            'request': {
+                'method': 'GET',
+                'params': {
+                    "deprecated": "fixture"
+                },
+                'headers': {
+                    'Authorization': ['Bearer abcde'],
+                    'Content-Type': ['application/json']
+                },
+                'url': 'http://',
+            },
+            'response': {}
+        },
+
+        // 49:
+        {
+            'request': {
+                'method': 'GET',
+                'params': {
+                    "deprecated": "fixture"
+                },
+                'headers': {
+                    'Authorization': ['Bearer abcde'],
+                    'Content-Type': ['application/json']
+                },
+                'url': 'http://',
+            },
+            'response': {}
+        },
+
     ];
 };
