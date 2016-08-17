cebc448f3c94156cc45ed51dd04d00635df88029
NicolaasDieBaas <nicki@westerncapelabs.com>
2016-08-17 16:41:16 +0200

    improve IdentityStore fixture readability

--- test/fixtures_identity_store.js
+++ test/fixtures_identity_store.js
@@ -46,21 +46,23 @@ module.exports = function() {
                     "count": 1,
                     "next": null,
                     "previous": null,
-                    "results": [{
-                        "url": "http://is/api/v1/identities/cb245673-aa41-4302-ac47-00000001002/",
-                        "id": "cb245673-aa41-4302-ac47-00000001002",
-                        "version": 1,
-                        "details": {
-                            "default_addr_type": "msisdn",
-                            "addresses": {
-                                "msisdn": {
-                                    "+27820001002": {}
+                    "results": [
+                        {
+                            "url": "http://is/api/v1/identities/cb245673-aa41-4302-ac47-00000001002/",
+                            "id": "cb245673-aa41-4302-ac47-00000001002",
+                            "version": 1,
+                            "details": {
+                                "default_addr_type": "msisdn",
+                                "addresses": {
+                                    "msisdn": {
+                                        "+27820001002": {}
+                                    }
                                 }
-                            }
-                        },
-                        "created_at": "2016-08-05T06:13:29.693272Z",
-                        "updated_at": "2016-08-05T06:13:29.693298Z"
-                    }]
+                            },
+                            "created_at": "2016-08-05T06:13:29.693272Z",
+                            "updated_at": "2016-08-05T06:13:29.693298Z"
+                        }
+                    ]
                 }
             }
         },
@@ -82,31 +84,33 @@ module.exports = function() {
                     "count": 1,
                     "next": null,
                     "previous": null,
-                    "results": [{
-                        "url": "http://is/api/v1/identities/cb245673-aa41-4302-ac47-00000001003/",
-                        "id": "cb245673-aa41-4302-ac47-00000001003",
-                        "version": 1,
-                        "details": {
-                            "default_addr_type": "msisdn",
-                            "addresses": {
-                                "msisdn": {
-                                    "+27820001003": {}
+                    "results": [
+                        {
+                            "url": "http://is/api/v1/identities/cb245673-aa41-4302-ac47-00000001003/",
+                            "id": "cb245673-aa41-4302-ac47-00000001003",
+                            "version": 1,
+                            "details": {
+                                "default_addr_type": "msisdn",
+                                "addresses": {
+                                    "msisdn": {
+                                        "+27820001003": {}
+                                    }
+                                },
+                                "nurseconnect": {
+                                    "last_reg_id": "7",
+                                    "is_registered": 'true',
+                                    "faccode": '123456',
+                                    "facname": 'WCL clinic',
+                                    "working_on": "",
+                                    "id_type": "sa_id",
+                                    "sa_id_no": "5101025009086",
+                                    "dob": "1951-01-02",
                                 }
                             },
-                            "nurseconnect": {
-                                "last_reg_id": "7",
-                                "is_registered": 'true',
-                                "faccode": '123456',
-                                "facname": 'WCL clinic',
-                                "working_on": "",
-                                "id_type": "sa_id",
-                                "sa_id_no": "5101025009086",
-                                "dob": "1951-01-02",
-                            }
-                        },
-                        "created_at": "2016-08-05T06:13:29.693272Z",
-                        "updated_at": "2016-08-05T06:13:29.693298Z"
-                    }]
+                            "created_at": "2016-08-05T06:13:29.693272Z",
+                            "updated_at": "2016-08-05T06:13:29.693298Z"
+                        }
+                    ]
                 }
             }
         },
