cdc2399735ab9eb36bdde5611aa28e22595b1bf1
NicolaasDieBaas <nicki@westerncapelabs.com>
2016-09-01 10:28:10 +0200

    add logging property to im.config

--- test/sms_inbound.test.js
+++ test/sms_inbound.test.js
@@ -76,7 +76,8 @@ describe("app", function() {
                         three_per_week: 4,
                         four_per_week: 5,
                         five_per_week: 6
-                    }
+                    },
+                    logging: 'off'
                 })
                 .setup(function(api) {
                     api.kv.store['test.smsinbound.unique_users'] = 0;
