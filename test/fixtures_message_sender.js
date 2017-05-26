a7fc141ed7839fba0de13d1065eeb4809b92b6bf
Carla Wilby <thisiscarlawilby@gmail.com>
2017-05-26 09:40:09 +0200

    Fixing tiny error

--- test/fixtures_message_sender.js
+++ test/fixtures_message_sender.js
@@ -948,7 +948,7 @@ module.exports = function() {
                 "url": 'http://ms/api/v1/outbound/',
                 "method": 'POST',
                 "data": {
-                    "identity": "cb245673-aa41-4302-ac47-00000001002",
+                    "to_identity": "cb245673-aa41-4302-ac47-00000001002",
                     "to_addr": "+27820001002",
                     "content": "Personal info:\nPhone Number: +27820001002\nID Number: 5101025009086\nDate of Birth: 1951-01-02\nLanguage: \nMessage set: momconnect_prebirth.hw_full.1",                    
                     "metadata":{}
@@ -960,24 +960,15 @@ module.exports = function() {
             }
         },
 
-        // 142: post outbound message for cb245673-aa41-4302-ac47-00000001013
+        // 142:
         {
-            "key": "post.ms.outbound.27820001013",
-            "request": {
-                "url": 'http://ms/api/v1/outbound/',
-                "method": 'POST',
-                "data": {
-                    "identity": "cb245673-aa41-4302-ac47-00000001013",
-                    "to_addr": "+27820001013",
-                    "content": "Personal info:\nPhone #: +27820001013\nID: 5101025009086\nDOB: 1951-01-02\nLanguage: \nMessage set: momconnect_prebirth.hw_full.1",                    
-                    "metadata":{}
-                }
+            'request': {
+                'method': 'GET',
+                'url': 'http://',
             },
-                "response": {
-                "code": 201,
-                "data": {}
-            }
+            'response': {}
         },
+
         // 143:
         {
             'request': {
