638084bc106e58ea97eed366d13b0005210e2daf
Rudi Giesler <rudi@praekelt.org>
2017-06-14 17:35:15 +0200

    Add test for setting user's language from identity store on popi faq

--- src/ussd_popi_faq.js
+++ src/ussd_popi_faq.js
@@ -93,7 +93,7 @@ go.app = function() {
             .then(function(identity) {
                 self.im.user.set_answer("operator", identity);
                 self.im.user.set_answer("msisdn", msisdn);
-                self.im.set_lang(self.im.user.answers.operator.details.lang_code);
+                self.im.user.set_lang(self.im.user.answers.operator.details.lang_code);
 
 
                 return sbm
