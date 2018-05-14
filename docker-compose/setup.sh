950cbf25de45da8738a6830d60feaa1f6db8c7ea
Rudi Giesler <rudi@praekelt.org>
2018-05-14 17:05:44 +0200

    Separate DB for each service

--- docker-compose/setup.sh
+++ docker-compose/setup.sh
@@ -2,20 +2,36 @@
 set -e
 
 ## Create the required models
-# auth
+# identitystore
 echo "
 from django.contrib.auth.models import User
 user = User.objects.create_superuser('admin', 'admin@example.org', 'admin')
 from rest_framework.authtoken.models import Token
 Token.objects.create(user=user, key='b229801c76c68fb5b70075d08182bf1de681bb87')
 " | docker-compose run identitystore django-admin shell
-# registration source
+# registration
 echo "
 from django.contrib.auth.models import User
-user = User.objects.get(username='admin')
+user = User.objects.create_superuser('admin', 'admin@example.org', 'admin')
+from rest_framework.authtoken.models import Token
+Token.objects.create(user=user, key='b229801c76c68fb5b70075d08182bf1de681bb87')
 from registrations.models import Source
 Source.objects.create(user=user, authority='hw_full')
 " | docker-compose run hub django-admin shell
+# stage based messaging
+echo "
+from django.contrib.auth.models import User
+user = User.objects.create_superuser('admin', 'admin@example.org', 'admin')
+from rest_framework.authtoken.models import Token
+Token.objects.create(user=user, key='b229801c76c68fb5b70075d08182bf1de681bb87')
+" | docker-compose run sbm django-admin shell
+# message sender
+echo "
+from django.contrib.auth.models import User
+user = User.objects.create_superuser('admin', 'admin@example.org', 'admin')
+from rest_framework.authtoken.models import Token
+Token.objects.create(user=user, key='b229801c76c68fb5b70075d08182bf1de681bb87')
+" | docker-compose run messagesender django-admin shell
 
 # Create the Junebug channels
 curl -X POST -H 'Content-Type: application/json' -d '{
