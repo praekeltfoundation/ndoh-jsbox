0abc883d40f0cf8184b279665660578e937481b9
Onnie Mosimege <onniemosimege@Onnies-MacBook-Pro.local>
2018-06-27 08:30:53 +0200

    added states for non subscribed nurses

--- src/ussd_nurse_rapidpro.js
+++ src/ussd_nurse_rapidpro.js
@@ -1,6 +1,7 @@
 go.app = function() {
     var vumigo = require('vumigo_v02');
     var MenuState = vumigo.states.MenuState;
+    var ChoiceState = vumigo.states.ChoiceState;
     var FreeText = vumigo.states.FreeText;
     var Choice = vumigo.states.Choice;
     var SeedJsboxUtils = require('seed-jsbox-utils');
@@ -14,6 +15,7 @@ go.app = function() {
 
         //variables for services
         var is;
+
         self.init = function() {
             // initialise services
             is = new IdentityStore(
@@ -24,18 +26,19 @@ go.app = function() {
         };
 
         self.states.add("state_start", function(name) {
-            self.im.user.set_answers = {};
+            // Reset user answers when restarting the app
+            self.im.user.answers = {};
             var msisdn = utils.normalize_msisdn(self.im.user.addr, '27');
             self.im.user.set_answer("operator_msisdn", msisdn);
-            return is
-            .get_or_create_identity({"msisdn": msisdn})
-            .then(function(identity) {
-                self.im.user.set_answer("operator", identity);
-                self.im.user.set_answer("msisdn",msisdn);
-            })
-            .then(function() {
-                return self.states.create('state_not_registered');
-            });
+
+            /*
+                find identity & check subscription using CompanionApp
+
+                will go to state_registered if found,
+                else state_not_registered
+            */
+            return self.states.create('state_not_registered');
+        
         });
 
         self.states.add('state_not_registered', function(name){
@@ -58,7 +61,51 @@ go.app = function() {
             });
         });
 
-        
+        self.states.add('state_weekly_messages', function(name) {
+            return new ChoiceState(name, {
+                question: ("To register, your info needs to be collected, stored and used. " +
+                           "You might also receive messages on public holidays. Do you agree?"),
+                choices: [
+                    new Choice('yes', ('Yes')),
+                    new Choice('no', ('No')),
+                ],
+                next: function(choice) {
+                    if (choice.value === 'yes') {
+                        return 'state_agree_to_register';
+                    } else {
+                        return 'state_no_registration';
+                    }
+                }
+            });
+        });
+
+        self.states.add('state_friend_register', function(name) {
+            return new ChoiceState(name, {
+                question: ("To register, your friend's info needs to be collected, stored and used. "+
+                            "They may receive messages on public holidays. Do they agree?"),
+                choices: [
+                    new Choice('yes', ('Yes')),
+                    new Choice('no', ('No')),
+                ],
+                next: function(choice) {
+                    if (choice.value === 'yes') {
+                        return 'state_agree_to_register';
+                    } else {
+                        return 'state_no_registration';
+                    }
+                }
+            });
+        });
+
+        self.states.add('state_no_registration', function(name) {
+            return new MenuState(name, {
+                question: ("If you/they don't agree to share info, we can't send NurseConnect messages. " +
+                           "Reply '1' if you/they change your/their mind and would like to sign up."),
+                choices: [
+                    new Choice('state_nurse_connect_options', ('Main Menu')),
+                ],
+            });
+        });
 
 
     });
