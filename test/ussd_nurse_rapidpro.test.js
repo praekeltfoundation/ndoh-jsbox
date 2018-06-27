7e109e8388dec0a90f09c2d742c6a168387cbcfd
Onnie Mosimege <onniemosimege@Onnies-MacBook-Pro.local>
2018-06-27 08:31:51 +0200

    added tests for states for non subscribed nurses

--- test/ussd_nurse_rapidpro.test.js
+++ test/ussd_nurse_rapidpro.test.js
@@ -30,7 +30,7 @@ describe('app', function() {
                 .setup(function(api) {
                     // add fixtures for services used
                     fixtures_IdentityStore().forEach(api.http.fixtures.add); // 120 ->
-                    _.map(['+27820001001', '+27820001002', '+27820001013'], function(address) {
+                    _.map(['+27820001001', '+27820001003'], function(address) {
                         api.http.fixtures.add(
                             fixtures_Pilot().not_exists({
                                 number: '+27123456789',
@@ -50,6 +50,7 @@ describe('app', function() {
   
 
         describe('state_start', function() {
+
             describe('user not registered on nurseconnect', function() {
                 it('should go to state_not_registered', function() {
                     return tester
@@ -67,28 +68,113 @@ describe('app', function() {
                     .run();
                 });
             });
-        });
 
-        describe('user chooses to start NurseConnect', function(){
-            it("give NurseConnect options", function(){
-              return tester
-              .setup.user.addr('27820001001')
-              .inputs(
-                  {session_event: 'new'},
-                  "1"
-              )
-              .check.interaction({
-                state: 'state_nurse_connect_options',
-                reply: [
-                    "Do you want to:",
-                    "1. Sign up for weekly messages",
-                    "2. Change your no",
-                    "3. Help a friend register",
-                  ].join('\n')
-              })
-              .run();
-            });
-          });
+            describe('user chooses to start NurseConnect', function(){
+                it("give NurseConnect options", function(){
+                  return tester
+                  .setup.user.addr('27820001001')
+                  .inputs(
+                      {session_event: 'new'},
+                      "1"
+                  )
+                  .check.interaction({
+                    state: 'state_nurse_connect_options',
+                    reply: [
+                        "Do you want to:",
+                        "1. Sign up for weekly messages",
+                        "2. Change your no",
+                        "3. Help a friend register",
+                      ].join('\n')
+                  })
+                  .run();
+                });
+
+                describe('user wants to sign up for weekly messages', function(){
+                    it("should go to state_weekly_messages", function(){
+                        return tester
+                        .setup.user.addr('27820001001')
+                        .inputs(
+                            {session_event: 'new'},
+                            "1", //chooses to start nurse connect
+                            "1"  // chooses to sign up for weekly messages
+                        )
+                        .check.interaction({
+                            state: 'state_weekly_messages',
+                            reply: ["To register, your info needs to be collected, stored and used. " +
+                                    "You might also receive messages on public holidays. Do you agree?",
+                                    "1. Yes",
+                                    "2. No"
+                            ].join('\n')
+                        })
+                        .run();
+                    });
+
+                    describe('user disagrees to registration terms', function(){
+                        it("should go to state_no_registration", function(){
+                            return tester
+                            .setup.user.addr('27820001001')
+                            .inputs(
+                                {session_event: 'new'},
+                                "1", //chooses to start nurse connect
+                                "1",  // chooses to sign up for weekly messages
+                                "2" //chooses no
+                            )
+                            .check.interaction({
+                                state: 'state_no_registration',
+                                reply: ["If you/they don't agree to share info, we can't send NurseConnect messages. " +
+                                        "Reply '1' if you/they change your/their mind and would like to sign up.",
+                                        "1. Main Menu"
+                                ].join('\n')
+                            })
+                            .run();
+                        });
+                    });
+                });
+
+                describe('user wants to help a friend register', function(){
+                    it("should state_friend_register", function(){
+                        return tester
+                        .setup.user.addr('27820001001')
+                        .inputs(
+                            {session_event: 'new'},
+                            "1", //chooses to start nurse connect
+                            "3"  // chooses to help a friend register
+                        )
+                        .check.interaction({
+                            state: 'state_friend_register',
+                            reply: ["To register, your friend's info needs to be collected, stored and used. " +
+                                    "They may receive messages on public holidays. Do they agree?",
+                                    "1. Yes",
+                                    "2. No"
+                            ].join('\n')
+                        })
+                        .run();
+                    });
+
+                    describe('user disagrees to registration terms', function(){
+                        it("should go to state_no_registration", function(){
+                            return tester
+                            .setup.user.addr('27820001001')
+                            .inputs(
+                                {session_event: 'new'},
+                                "1", //chooses to start nurse connect
+                                "1",  // chooses to sign up for weekly messages
+                                "2" //chooses no
+                            )
+                            .check.interaction({
+                                state: 'state_no_registration',
+                                reply: ["If you/they don't agree to share info, we can't send NurseConnect messages. " +
+                                        "Reply '1' if you/they change your/their mind and would like to sign up.",
+                                        "1. Main Menu"
+                                ].join('\n')
+                            })
+                            .run();
+                        });
+                    });
+                });
+              }); //end of start NurseConnect
+                 
+        }); //end of state_start
     
     
     });
