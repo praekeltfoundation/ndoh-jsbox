4545a36bc5853387e00b7b8c1d6c855600c43eb1
Rudi Giesler <rudi@praekelt.org>
2018-08-03 11:37:35 +0200

    Fix submit NurseConnect registration to OpenHIM

--- go-app-ussd_nurse_rapidpro.js
+++ go-app-ussd_nurse_rapidpro.js
@@ -102,10 +102,11 @@ go.OpenHIM = function() {
     var utils = SeedJsboxUtils.utils;
     var moment = require('moment');
 
-    var OpenHIM = Eventable.extend(function(self, json_api, base_url, username, password) {
-        self.json_api = json_api;
+    var OpenHIM = Eventable.extend(function(self, http_api, base_url, username, password) {
+        self.http_api = http_api;
         self.base_url = base_url;
-        self.json_api.defaults.auth = {username: username, password: password};
+        self.http_api.defaults.auth = {username: username, password: password};
+        self.http_api.defaults.headers['Content-Type'] = ['application/json; charset=utf-8'];
 
         self.validate_nc_clinic_code = function(clinic_code) {
             /* Returns the clinic name if clinic code is valid, otherwise returns false */
@@ -117,9 +118,10 @@ go.OpenHIM = function() {
                 var params = {
                     criteria: "value:" + clinic_code
                 };
-                return self.json_api.get(url, {params: params})
-                .then(function(json_result) {
-                    var rows = json_result.data.rows;
+                return self.http_api.get(url, {params: params})
+                .then(function(result) {
+                    result = JSON.parse(result.body);
+                    var rows = result.rows;
                     if (rows.length === 0) {
                         return false;
                     } else {
@@ -131,7 +133,7 @@ go.OpenHIM = function() {
 
         self.submit_nc_registration = function(contact) {
             var url = self.base_url + "nc/subscription";
-            return self.json_api.post(url, {data: {
+            return self.http_api.post(url, {data: JSON.stringify({
                 mha: 1,
                 swt: contact.fields.preferred_channel === "whatsapp" ? 7 : 3,
                 type: 7,
@@ -144,7 +146,7 @@ go.OpenHIM = function() {
                 persal: contact.fields.persal || null,
                 sanc: contact.fields.sanc || null,
                 encdate: moment(contact.fields.registration_date).utc().format('YYYYMMDDHHmmss')
-            }});
+            })});
         };
     });
 
@@ -162,6 +164,7 @@ go.app = function() {
     var Choice = vumigo.states.Choice;
     var SeedJsboxUtils = require('seed-jsbox-utils');
     var JsonApi = vumigo.http.api.JsonApi;
+    var HttpApi = vumigo.http.api.HttpApi;
     var utils = SeedJsboxUtils.utils;
     var App = vumigo.App;
     var moment = require('moment');
@@ -179,7 +182,7 @@ go.app = function() {
             );
 
             self.openhim = new go.OpenHIM(
-                new JsonApi(self.im, {}),
+                new HttpApi(self.im, {}),
                 self.im.config.services.openhim.url_json,
                 self.im.config.services.openhim.username,
                 self.im.config.services.openhim.password
--- src/openhim.js
+++ src/openhim.js
@@ -7,10 +7,11 @@ go.OpenHIM = function() {
     var utils = SeedJsboxUtils.utils;
     var moment = require('moment');
 
-    var OpenHIM = Eventable.extend(function(self, json_api, base_url, username, password) {
-        self.json_api = json_api;
+    var OpenHIM = Eventable.extend(function(self, http_api, base_url, username, password) {
+        self.http_api = http_api;
         self.base_url = base_url;
-        self.json_api.defaults.auth = {username: username, password: password};
+        self.http_api.defaults.auth = {username: username, password: password};
+        self.http_api.defaults.headers['Content-Type'] = ['application/json; charset=utf-8'];
 
         self.validate_nc_clinic_code = function(clinic_code) {
             /* Returns the clinic name if clinic code is valid, otherwise returns false */
@@ -22,9 +23,10 @@ go.OpenHIM = function() {
                 var params = {
                     criteria: "value:" + clinic_code
                 };
-                return self.json_api.get(url, {params: params})
-                .then(function(json_result) {
-                    var rows = json_result.data.rows;
+                return self.http_api.get(url, {params: params})
+                .then(function(result) {
+                    result = JSON.parse(result.body);
+                    var rows = result.rows;
                     if (rows.length === 0) {
                         return false;
                     } else {
@@ -36,7 +38,7 @@ go.OpenHIM = function() {
 
         self.submit_nc_registration = function(contact) {
             var url = self.base_url + "nc/subscription";
-            return self.json_api.post(url, {data: {
+            return self.http_api.post(url, {data: JSON.stringify({
                 mha: 1,
                 swt: contact.fields.preferred_channel === "whatsapp" ? 7 : 3,
                 type: 7,
@@ -49,7 +51,7 @@ go.OpenHIM = function() {
                 persal: contact.fields.persal || null,
                 sanc: contact.fields.sanc || null,
                 encdate: moment(contact.fields.registration_date).utc().format('YYYYMMDDHHmmss')
-            }});
+            })});
         };
     });
 
