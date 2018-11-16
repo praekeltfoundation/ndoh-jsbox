5603932e94fb24c0690b8cef9272b4f4a685e2f3
Erik Harding <erik@praekelt.org>
2018-11-16 09:19:48 +0200

    remove unused vumi test

--- test/ussd_pmtct_seed.test.js
+++ test/ussd_pmtct_seed.test.js
@@ -1,6 +1,5 @@
 var vumigo = require("vumigo_v02");
 var AppTester = vumigo.AppTester;
-var assert = require('assert');
 
 var fixtures_IdentityStore = require('./fixtures_identity_store');
 var fixtures_StageBasedMessaging = require('./fixtures_stage_based_messaging');
@@ -12,18 +11,6 @@ var fixtures_IdentityStoreDynamic = require('./fixtures_identity_store_dynamic')
 var fixtures_StageBasedMessagingDynamic = require('./fixtures_stage_based_messaging_dynamic')();
 
 var utils = require('seed-jsbox-utils').utils;
-
-describe("Test messageset regex using in state_get_vumi_contact", function() {
-    it("should get the messageset id from the url", function() {
-        // valid url strings
-        assert.equal("www.foo.com/v1/go/messageset/1/".match(/\d+\/$/)[0].replace('/', ''), "1");
-        assert.equal("www.foo.com/v1/go/messageset/12/".match(/\d+\/$/)[0].replace('/', ''), "12");
-        assert.equal("www.foo.com/v1/go/messageset/12345/".match(/\d+\/$/)[0].replace('/', ''), "12345");
-        // invalid url strings
-        assert.equal("www.foo.com/v1/go/messageset/1two3/".match(/\d+\/$/)[0].replace('/', ''), "3");
-        assert.equal("www.foo.com/v1/go/messageset/one/".match(/\d+\/$/), null);
-    });
-});
 
 describe("PMTCT app", function() {
     describe("for ussd use - auth on", function() {
