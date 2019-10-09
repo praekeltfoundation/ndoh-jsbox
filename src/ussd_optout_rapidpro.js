1a94dead3f4101cae1998f9a2a1f9d68914d01d0
Rudi Giesler <rudi@praekelt.org>
2019-10-09 14:39:17 +0200

    Add basic setup for rapidpro optout ussd line

---
+++ src/ussd_optout_rapidpro.js
@@ -0,0 +1,32 @@
+go.app = function() {
+    var vumigo = require("vumigo_v02");
+    var App = vumigo.App;
+    var ChoiceState = vumigo.states.ChoiceState;
+    var Choice = vumigo.states.Choice;
+
+    var GoNDOH = App.extend(function(self) {
+        App.call(self, "state_start");
+        var $ = self.$;
+
+        self.init = function() {
+        };
+
+        self.states.add("state_start", function(name) {
+            return new ChoiceState(name, {
+                question: $("Please let us know why you do not want MomConnect messages"),
+                choices: [
+                    new Choice("miscarriage", $("Miscarriage")),
+                    new Choice("stillbirth", $("Baby was stillborn")),
+                    new Choice("babyloss", $("Baby died")),
+                    new Choice("not_useful", $("Messages not useful")),
+                    new Choice("other", $("Other"))
+                ],
+                next: "state_start"
+            });
+        });
+    });
+
+    return {
+        GoNDOH: GoNDOH
+    };
+}();
