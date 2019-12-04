784dff51bc21cb9bf31c9131ebf6996d429e00ac
Rudi Giesler <rudi@praekelt.org>
2019-12-04 17:15:59 +0200

    Add MSISDN entering screen

--- go-app-ussd_clinic_rapidpro.js
+++ go-app-ussd_clinic_rapidpro.js
@@ -3,9 +3,10 @@ go;
 
 go.app = function() {
     var vumigo = require("vumigo_v02");
-    var utils = require('seed-jsbox-utils').utils;
+    var utils = require("seed-jsbox-utils").utils;
     var App = vumigo.App;
     var Choice = vumigo.states.Choice;
+    var FreeText = vumigo.states.FreeText;
     var MenuState = vumigo.states.MenuState;
 
 
@@ -28,6 +29,29 @@ go.app = function() {
                     new Choice("state_enter_msisdn", "No")
                 ]
             });
+        });
+
+        self.states.add("state_enter_msisdn", function(name) {
+            return new FreeText(name, {
+                question: $(
+                    "Please enter the cell number of the mother who would like to sign up to " +
+                    "receive messages from MomConnect, e.g. 0813547654."),
+                check: function(content) {
+                    if(!utils.is_valid_msisdn(content, "ZA")) {
+                        return $(
+                            "Sorry, we don't understand that cell number. Please enter 10 digit " +
+                            "cell number that the mother would like to get MomConnect messages " +
+                            "on, e.g. 0813547654.");
+                    }
+                    if(utils.normalize_msisdn(content, "ZA") === "+27813547654") {
+                        return $(
+                            "We're looking for the mother's information. Please avoid entering " +
+                            "the examples in the messages. Enter the mother's details."
+                        );
+                    }
+                },
+                next: "state_get_contact"
+            })
         });
     });
 
--- test/ussd_clinic_rapidpro.test.js
+++ test/ussd_clinic_rapidpro.test.js
@@ -28,4 +28,46 @@ describe("ussd_public app", function() {
                 .run();
         });
     });
+
+    describe("state_enter_msisdn", function() {
+        it("should ask the user for the mother's number", function() {
+            return tester
+                .setup.user.state("state_enter_msisdn")
+                .check.interaction({
+                    state: "state_enter_msisdn",
+                    reply: (
+                        "Please enter the cell number of the mother who would like to sign up " +
+                        "to receive messages from MomConnect, e.g. 0813547654."
+                    )
+                })
+                .run();
+        });
+        it("should not allow invalid msisdns", function() {
+            return tester
+                .setup.user.state("state_enter_msisdn")
+                .input("123abc")
+                .check.interaction({
+                    state: "state_enter_msisdn",
+                    reply: (
+                        "Sorry, we don't understand that cell number. Please enter 10 digit cell " +
+                        "number that the mother would like to get MomConnect messages on, " +
+                        "e.g. 0813547654."
+                    )
+                })
+                .run();
+        });
+        it("should not allow the example msisdn", function() {
+            return tester
+                .setup.user.state("state_enter_msisdn")
+                .input("0813547654")
+                .check.interaction({
+                    state: "state_enter_msisdn",
+                    reply: (
+                        "We're looking for the mother's information. Please avoid entering the " +
+                        "examples in the messages. Enter the mother's details."
+                    )
+                })
+                .run();
+        });
+    });
 });
