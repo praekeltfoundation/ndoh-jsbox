0583b80a165d628e9c665d9af70cbd9dc3200c73
Erik Harding <erik@praekelt.org>
2020-04-06 12:50:37 +0200

    remove WA and replace rapidpro with eventstore

--- test/ussd_ccmdd_wc_address_update.test.js
+++ test/ussd_ccmdd_wc_address_update.test.js
@@ -1,57 +1,38 @@
 var vumigo = require("vumigo_v02");
 var AppTester = vumigo.AppTester;
-var fixtures_rapidpro = require("./fixtures_rapidpro")();
-var fixtures_whatsapp = require("./fixtures_pilot")();
 
-describe("ussd_ccmdd_wc_address_update app", function() {
+describe("ussd_ccmdd_wc_address_update app", function () {
   var app;
   var tester;
 
-  beforeEach(function() {
+  beforeEach(function () {
     app = new go.app.GoNDOH();
     tester = new AppTester(app);
     tester.setup.config.app({
-      services: {
-        rapidpro: {
-          base_url: "https://rapidpro",
-          token: "rapidpro-token"
-        },
-        whatsapp: {
-          base_url: "http://pilot.example.org",
-          token: "engage-token"
-        }
+      eventstore: {
+        url: "http://eventstore",
+        token: "testtoken",
       },
-      flow_uuid: "rapidpro-flow-uuid"
     });
   });
-  describe("state_start", function() {
-    it("should give a welcome message", function() {
+  describe("state_start", function () {
+    it("should give a welcome message", function () {
       return tester
-        .setup(function(api) {
-          api.http.fixtures.add(
-            fixtures_rapidpro.get_contact({
-              urn: "whatsapp:27820001001",
-              groups: [],
-              exists: true
-            })
-          );
-        })
-        .setup.user.addr("27820001001")
         .start()
         .check.interaction({
           state: "state_start",
           reply: [
             "Welcome to the Department of Health's Medication Home Delivery Service.",
             "We deliver prescription meds to your door.",
-            "1. Continue"
+            "1. Continue",
           ].join("\n"),
-          char_limit: 140
+          char_limit: 140,
         })
         .run();
     });
   });
-  describe("state_info_consent", function() {
-    it("should ask consent to collect personal info", function() {
+  describe("state_info_consent", function () {
+    it("should ask consent to collect personal info", function () {
       return tester.setup.user
         .state("state_info_consent")
         .check.interaction({
@@ -60,13 +41,13 @@ describe("ussd_ccmdd_wc_address_update app", function() {
             "To have your prescription medication delivered to your door, we need to process your personal info.",
             "Do you want to continue?",
             "1. Yes",
-            "2. No"
+            "2. No",
           ].join("\n"),
-          char_limit: 160
+          char_limit: 160,
         })
         .run();
     });
-    it("should display an error message on incorrect input", function() {
+    it("should display an error message on incorrect input", function () {
       return tester.setup.user
         .state("state_info_consent")
         .input("a")
@@ -75,39 +56,39 @@ describe("ussd_ccmdd_wc_address_update app", function() {
           reply: [
             "Sorry we don't understand. Please enter the number next to your answer.",
             "1. Yes",
-            "2. No"
+            "2. No",
           ].join("\n"),
-          char_limit: 140
+          char_limit: 140,
         })
         .run();
     });
   });
-  describe("state_first_name", function() {
-    it("should ask for first name", function() {
+  describe("state_first_name", function () {
+    it("should ask for first name", function () {
       return tester.setup.user
         .state("state_first_name")
         .check.interaction({
           state: "state_first_name",
           reply: "[1/10] What is your first name?",
-          char_limit: 160
+          char_limit: 160,
         })
         .run();
     });
   });
-  describe("state_surname", function() {
-    it("should ask for surname", function() {
+  describe("state_surname", function () {
+    it("should ask for surname", function () {
       return tester.setup.user
         .state("state_surname")
         .check.interaction({
           state: "state_surname",
           reply: "[2/10] What is your last name?",
-          char_limit: 160
+          char_limit: 160,
         })
         .run();
     });
   });
-  describe("state_id_type", function() {
-    it("should ask what id type is available", function() {
+  describe("state_id_type", function () {
+    it("should ask what id type is available", function () {
       return tester.setup.user
         .state("state_id_type")
         .check.interaction({
@@ -115,41 +96,41 @@ describe("ussd_ccmdd_wc_address_update app", function() {
           reply: [
             "[3/10] What type of identification do you have?",
             "1. SA ID",
-            "2. None"
+            "2. None",
           ].join("\n"),
-          char_limit: 160
+          char_limit: 160,
         })
         .run();
     });
   });
-  describe("state_sa_id_no", function() {
-    it("should ask for the id number", function() {
+  describe("state_sa_id_no", function () {
+    it("should ask for the id number", function () {
       return tester.setup.user
         .state("state_sa_id_no")
         .check.interaction({
           state: "state_sa_id_no",
           reply:
             "[4/10] Please reply with your ID number as you find it in your Identity Document.",
-          char_limit: 160
+          char_limit: 160,
         })
         .run();
     });
   });
-  describe("state_dob_year", function() {
-    it("should ask what year the user was born", function() {
+  describe("state_dob_year", function () {
+    it("should ask what year the user was born", function () {
       return tester.setup.user
         .state("state_dob_year")
         .check.interaction({
           state: "state_dob_year",
           reply:
             "[4/10] What year were you born? Please reply with the year as 4 digits in the format YYYY.",
-          char_limit: 160
+          char_limit: 160,
         })
         .run();
     });
   });
-  describe("state_dob_month", function() {
-    it("should ask which month the user was born", function() {
+  describe("state_dob_month", function () {
+    it("should ask which month the user was born", function () {
       return tester.setup.user
         .state("state_dob_month")
         .check.interaction({
@@ -167,13 +148,13 @@ describe("ussd_ccmdd_wc_address_update app", function() {
             "9. Sep",
             "10. Oct",
             "11. Nov",
-            "12. Dec"
+            "12. Dec",
           ].join("\n"),
-          char_limit: 160
+          char_limit: 160,
         })
         .run();
     });
-    it("should accept a label", function() {
+    it("should accept a label", function () {
       return tester.setup.user
         .state("state_dob_month")
         .input("jan")
@@ -181,40 +162,40 @@ describe("ussd_ccmdd_wc_address_update app", function() {
           state: "state_dob_day",
           reply:
             "[4/10] On what day were you born? Please enter the day as a number, e.g. 12.",
-          char_limit: 160
+          char_limit: 160,
         })
         .check.user.answer("state_dob_month", "01")
         .run();
     });
   });
-  describe("state_dob_day", function() {
-    it("should ask what ay the user was born", function() {
+  describe("state_dob_day", function () {
+    it("should ask what ay the user was born", function () {
       return tester.setup.user
         .state("state_dob_day")
         .check.interaction({
           state: "state_dob_day",
           reply:
             "[4/10] On what day were you born? Please enter the day as a number, e.g. 12.",
-          char_limit: 160
+          char_limit: 160,
         })
         .run();
     });
   });
-  describe("state_folder_number", function() {
-    it("should ask for the folder number", function() {
+  describe("state_folder_number", function () {
+    it("should ask for the folder number", function () {
       return tester.setup.user
         .state("state_folder_number")
         .check.interaction({
           state: "state_folder_number",
           reply:
             "[5/10] Please reply with your folder number as you find it on your appointment card, e.g. 12345678",
-          char_limit: 160
+          char_limit: 160,
         })
         .run();
     });
   });
-  describe("state_municipality", function() {
-    it("should ask which municipality the user stays in", function() {
+  describe("state_municipality", function () {
+    it("should ask which municipality the user stays in", function () {
       return tester.setup.user
         .state("state_municipality")
         .check.interaction({
@@ -227,13 +208,13 @@ describe("ussd_ccmdd_wc_address_update app", function() {
             "4. Garden Route",
             "5. Overberg",
             "6. West Coast",
-            "7. None of the above"
+            "7. None of the above",
           ].join("\n"),
-          char_limit: 160
+          char_limit: 160,
         })
         .run();
     });
-    it("should save the correct value", function() {
+    it("should save the correct value", function () {
       return tester.setup.user
         .state("state_municipality")
         .input("2")
@@ -246,15 +227,15 @@ describe("ussd_ccmdd_wc_address_update app", function() {
             "3. Langeberg",
             "4. Stellenbosch",
             "5. Witzenberg",
-            "6. None of the above"
+            "6. None of the above",
           ].join("\n"),
-          char_limit: 160
+          char_limit: 160,
         })
         .run();
     });
   });
-  describe("state_city", function() {
-    it("should show the correct options for Cape winelands", function() {
+  describe("state_city", function () {
+    it("should show the correct options for Cape winelands", function () {
       return tester.setup.user
         .state("state_city")
         .setup.user.answer("state_municipality", "Cape Winelands")
@@ -267,13 +248,13 @@ describe("ussd_ccmdd_wc_address_update app", function() {
             "3. Langeberg",
             "4. Stellenbosch",
             "5. Witzenberg",
-            "6. None of the above"
+            "6. None of the above",
           ].join("\n"),
-          char_limit: 160
+          char_limit: 160,
         })
         .run();
     });
-    it("should show the correct options for Central Karoo", function() {
+    it("should show the correct options for Central Karoo", function () {
       return tester.setup.user
         .state("state_city")
         .setup.user.answer("state_municipality", "Central Karoo")
@@ -284,13 +265,13 @@ describe("ussd_ccmdd_wc_address_update app", function() {
             "1. Beaufort Wes",
             "2. Laingsburg",
             "3. Prince Albert",
-            "4. None of the above"
+            "4. None of the above",
           ].join("\n"),
-          char_limit: 160
+          char_limit: 160,
         })
         .run();
     });
-    it("should show the correct options for Garden Route", function() {
+    it("should show the correct options for Garden Route", function () {
       return tester.setup.user
         .state("state_city")
         .setup.user.answer("state_municipality", "Garden Route")
@@ -305,13 +286,13 @@ describe("ussd_ccmdd_wc_address_update app", function() {
             "5. Knysna",
             "6. Mosselbay",
             "7. Oudtshoorn",
-            "8. None of the above"
+            "8. None of the above",
           ].join("\n"),
-          char_limit: 160
+          char_limit: 160,
         })
         .run();
     });
-    it("should show the correct options for Overberg", function() {
+    it("should show the correct options for Overberg", function () {
       return tester.setup.user
         .state("state_city")
         .setup.user.answer("state_municipality", "Overberg")
@@ -323,13 +304,13 @@ describe("ussd_ccmdd_wc_address_update app", function() {
             "2. Overstrand",
             "3. Swellendam",
             "4. Theewaterskloof",
-            "5. None of the above"
+            "5. None of the above",
           ].join("\n"),
-          char_limit: 160
+          char_limit: 160,
         })
         .run();
     });
-    it("should show the correct options for West Coast", function() {
+    it("should show the correct options for West Coast", function () {
       return tester.setup.user
         .state("state_city")
         .setup.user.answer("state_municipality", "West Coast")
@@ -342,13 +323,13 @@ describe("ussd_ccmdd_wc_address_update app", function() {
             "3. Matzikama",
             "4. Saldanabay",
             "5. Swartland",
-            "6. None of the above"
+            "6. None of the above",
           ].join("\n"),
-          char_limit: 160
+          char_limit: 160,
         })
         .run();
     });
-    it("should save the correct value", function() {
+    it("should save the correct value", function () {
       return tester.setup.user
         .state("state_city")
         .setup.user.answer("state_municipality", "West Coast")
@@ -356,84 +337,80 @@ describe("ussd_ccmdd_wc_address_update app", function() {
         .check.interaction({
           state: "state_suburb",
           reply: "[8/10] Please reply with the name of your suburb.",
-          char_limit: 160
+          char_limit: 160,
         })
         .check.user.answer("state_city", "Cederberg")
         .run();
     });
   });
-  describe("state_suburb", function() {
-    it("should ask for the suburb", function() {
+  describe("state_suburb", function () {
+    it("should ask for the suburb", function () {
       return tester.setup.user
         .state("state_suburb")
         .check.interaction({
           state: "state_suburb",
           reply: "[8/10] Please reply with the name of your suburb.",
-          char_limit: 160
+          char_limit: 160,
         })
         .run();
     });
   });
-  describe("state_street_name", function() {
-    it("should ask for the street name", function() {
+  describe("state_street_name", function () {
+    it("should ask for the street name", function () {
       return tester.setup.user
         .state("state_street_name")
         .check.interaction({
           state: "state_street_name",
           reply: "[9/10] Please reply with the name of your street.",
-          char_limit: 160
+          char_limit: 160,
         })
         .run();
     });
   });
-  describe("state_street_number", function() {
-    it("should ask for the street number", function() {
+  describe("state_street_number", function () {
+    it("should ask for the street number", function () {
       return tester.setup.user
         .state("state_street_number")
         .check.interaction({
           state: "state_street_number",
           reply: "[10/10] Please reply with your house number, e.g. 17.",
-          char_limit: 160
+          char_limit: 160,
         })
         .run();
     });
   });
-  describe("state_whatsapp_contact_check", function() {
-    it("should continue and start rapdipro flow", function() {
+  describe("state_submit_data", function () {
+    it("should submit data to the eventstore", function () {
       return tester
-        .setup(function(api) {
-          api.http.fixtures.add(
-            fixtures_whatsapp.exists({
-              address: "+27820001003",
-              wait: true
-            })
-          );
-          api.http.fixtures.add(
-            fixtures_rapidpro.start_flow(
-              "rapidpro-flow-uuid",
-              null,
-              "whatsapp:27820001003",
-              {
-                source: "USSD address Update",
-                info_consent: "TRUE",
-                on_whatsapp: "TRUE",
+        .setup(function (api) {
+          api.http.fixtures.add({
+            request: {
+              url: "http://eventstore/api/v2/cduaddressupdate/",
+              method: "POST",
+              data: {
                 first_name: "Jane",
-                surname: "Smith",
+                last_name: "Smith",
                 id_type: "sa_id",
-                sa_id_number: "8811115022085",
-                dob: "1988-11-11T00:00:00Z",
+                id_number: "8811115022085",
+                date_of_birth: "1988-11-11T00:00:00Z",
                 folder_number: "12345678",
                 municipality: "Cape Winelands",
                 city: "Cape Town",
                 suburb: "Sea Point",
-                street_name: "High level stree",
-                street_number: "167"
-              }
-            )
-          );
+                street_name: "High level road",
+                street_number: "167",
+              },
+            },
+            response: {
+              code: 201,
+              data: {
+                accepted: true,
+              },
+            },
+          });
         })
-        .setup.user.addr("+27820001003")
-        .setup.user.state("state_whatsapp_contact_check")
+
+        .setup.user.state("state_submit_data")
         .setup.user.answer("state_first_name", "Jane")
         .setup.user.answer("state_surname", "Smith")
         .setup.user.answer("state_id_type", "state_sa_id_no")
@@ -442,13 +419,13 @@ describe("ussd_ccmdd_wc_address_update app", function() {
         .setup.user.answer("state_municipality", "Cape Winelands")
         .setup.user.answer("state_city", "Cape Town")
         .setup.user.answer("state_suburb", "Sea Point")
-        .setup.user.answer("state_street_name", "High level stree")
+        .setup.user.answer("state_street_name", "High level road")
         .setup.user.answer("state_street_number", "167")
         .check.interaction({
           state: "state_update_complete",
           reply:
             "Thank you. Your healthcare facility will be in contact with you soon about your medication delivery.",
-          char_limit: 160
+          char_limit: 160,
         })
         .run();
     });
